a9b1c12084c2c4c03afec912be2fb6df7ad6e890
DS4506 <davidsmith4506@gmail.com>
2025-09-22 21:38:30 -0500

    Initial Commit

---
+++ CryptoMagic/CryptoMagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoMagicApp.swift
+//  CryptoMagic
+//
+//  Created by Willie Earl on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoMagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
