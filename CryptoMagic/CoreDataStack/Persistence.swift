5d74fbbbe7b7b1f924b53ceeea03d3f5c558a4cf
DS4506 <davidsmith4506@gmail.com>
2025-09-26 20:12:30 -0700

    cryptoupdateSep25

--- CryptoMagic/CoreDataStack/Persistence.swift
+++ CryptoMagic/CoreDataStack/Persistence.swift
@@ -1,7 +1,25 @@
-//
-//  Persistence.swift
-//  CryptoMagic
-//
-//  Created by Willie Earl on 9/24/25.
-//
+import CoreData
 
+struct PersistenceController {
+    static let shared = PersistenceController()
+    
+    let container: NSPersistentContainer
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "Model")
+        if inMemory {
+            let description = NSPersistentStoreDescription()
+            description.type = NSInMemoryStoreType
+            container.persistentStoreDescriptions = [description]
+        }
+        container.loadPersistentStores{ _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        container.viewContext.automaticallyMergesChangeFromParent = true
+                                       
+    }
+    
+}
